552c5e1aa3cd7d8004ecc163e8cc1871ebfaec68
Abayomi Oyewumi <ace@syntaximagination.com>
2022-03-11 13:18:32 +0100

    Set UP Ready

---
+++ System/index.js
@@ -0,0 +1,12 @@
+//Definitions
+global._Helper = `${rootpath}/${config.folders.helpers}/Helper.js`;
+global._PayloadHelper = `${rootpath}/${config.folders.helpers}/Payload.js`;
+global._EncryptionHelper = `${rootpath}/${config.folders.helpers}/Encryption.js`;
+global._NetworkHelper = `${rootpath}/${config.folders.helpers}/Network.js`;
+global._MailHelper = `${rootpath}/${config.folders.helpers}/Mail.js`;
+global._ExtendedHelper = `${rootpath}/${config.folders.helpers}/Extended.js`;
+global._WebsocketHelper = `${rootpath}/${config.folders.helpers}/Websocket.js`;
+
+global._SecurityHelper = `${rootpath}/${config.folders.helpers}/Security/index.js`;
+
+global._ConfigFile = config.folders.config;
---
+++ base.js
@@ -0,0 +1,113 @@
+global.rootpath = __dirname;
+const { env } = require('process');
+
+const configFile = './bin/.config';
+
+const fs = require('fs');
+
+let config;
+
+config = fs.readFileSync(`${rootpath}/${configFile}`, 'utf-8');
+if (typeof (config) === 'string') {
+    config = JSON.parse(config);
+}
+
+let httpConfig, httpsConfig, wsConfig, wssConfig, socketIOConfig,
+    socketIOConnection = 0, webSocketConnection = 0;
+
+config.net.forEach(item => {
+    for (key in item) {
+        if (key === 'http') {
+            httpConfig = item;
+        } else if (key === 'https') {
+            httpsConfig = item;
+        } else if (key === 'ws') {
+            wsConfig = item;
+        } else if (key === 'wss') {
+            wssConfig = item;
+        } else if (key === 'socketio') {
+            socketIOConfig = item;
+        }
+    }
+});
+
+if (httpConfig.http === true || httpConfig.https === true) {
+
+    try {
+
+        //Globalize Config
+        global.config = config;
+
+        global.db = {};
+
+        //Connected to Database
+        config.db.forEach((db) => {
+            require(`${rootpath}/${config.folders.model}/${db.package}/${db.file}`)(db);
+        });
+
+        //Net Connections
+        config.net.forEach((net) => {
+
+            for (key in net) {
+                if (key !== 'data') {
+
+                    //HTTP
+                    if (key === 'http' && net[key] === true) {
+                        const http = require('http');
+                        const port = Number(net['data'].port);
+
+                        if (!isNaN(net['data'].port)) {
+
+
+                            const server = http.createServer((request, response) => {
+
+                                try {
+                                    require(`${rootpath}/${config.folders.config}/index.js`);
+                                    require(`${rootpath}/${config.folders.controller}/middleware.js`)(request, response);
+                                } catch (err) {
+                                    console.log(err);
+                                    console.log('Config Path Invalid');
+                                }
+                            });
+
+                            server.listen(port, () => {
+                                console.log(`Started on ${new Date()}`);
+                            });
+
+                        }
+                    }
+
+
+                }
+            }
+
+        });
+
+
+    } catch (e) {
+        console.log(e);
+        console.log('Error : Application cannot start, no config file defined / config file not found');
+    }
+
+}
+
+
+
+if (wsConfig.ws === true || wsConfig.wss === true) {
+    const Websocket = require(`${rootpath}/${config.folders.helpers}/Websocket.js`);
+    // const SocketIO = require(`${rootpath}/${config.folders.helpers}/SocketIO.js`);
+
+    if (wsConfig.ws === true) {
+        new Websocket().startSocket(wsConfig);
+        // new SocketIO().startServer(wsConfig);
+    }
+    else if (wsConfig.wss === true) {
+        new Websocket().startServer(wssConfig);
+    }
+
+}
+
+if (socketIOConfig.socketio === true) {
+    const SocketIO = require(`${rootpath}/${config.folders.helpers}/SocketIO.js`);
+    new SocketIO().startServer(socketIOConfig);
+}
