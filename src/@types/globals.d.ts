b5768aa5ce399c9927daa8643371eac985d12e4f
Abayomi Oyewumi <ace@syntaximagination.com>
2023-03-16 02:48:52 +0100

    Remove most global keys to Stoat Global Module

--- src/@types/globals.d.ts
+++ src/@types/globals.d.ts
@@ -1,31 +1,22 @@
 declare global {
-    type ConfigRecord = Record<string, any>;
-    type Staot = Record<string, any>;
+    
+    type Obj = Record<string, any>;
 
-    var __Stoat: Staot, //Global Stoat Variable 
+    var __Stoat: Obj, //Global Stoat Variable 
+    stoat : {
+        paths : Obj,
+        config : Obj,
+        db : Obj|[],
+        dynamicImport : Function
+    },
+
     __rootPath: string,
     __rootParent: string,
     
-    config:ConfigRecord,
-    db:[],
-
-    //Paths
-    _HelperDir: string,
-    _OthersDir: string,
-    
-    _Helper: string,
-    _PayloadHelper: string,
-    _EncryptionHelper: string,
-    _NetworkHelper: string,
-    _MailHelper: string,
-    _ExtendedHelper: string,
-    _WebsocketHelper: string,
-    _SecurityHelper: string,
-    _ConfigFile: string,
-
-    _PublicPath: string, 
+    db : [],
 
     //Stoat Global Variable
-    _stoatData:Record<string, any>
+    _stoatData:Obj
 }
+
 export {};
